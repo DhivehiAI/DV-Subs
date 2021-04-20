fa299e6962e5e90a9a6b5c613741c9cca76760f0
kudanai <kudanai@gmail.com>
2021-04-20 15:20:49 +0500

    change srt generator tweak defaults a bit

--- dv_subs.py
+++ dv_subs.py
@@ -2,7 +2,7 @@ import argparse
 from utils import *
 from tqdm import tqdm
 import shutil
-
+import srt
 
 def perc_float(x):
     try:
@@ -10,7 +10,7 @@ def perc_float(x):
     except ValueError:
         raise argparse.ArgumentTypeError("%r not a floating-point literal" % (x,))
 
-    if x < 0.0 or x > 1.0:
+    if x < 0.0 or x > 2.0:
         raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
     return x
 
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     arg_parse.add_argument("output", help="Output file name")
     arg_parse.add_argument("--model_dir", default="./models", help="STT model files directory")
     arg_parse.add_argument("--temp_dir", default="./temp", help="Temp files directory")
-    arg_parse.add_argument("--silence_window", default="0.3", type=perc_float, help="Audio smoothing window")
-    arg_parse.add_argument("--silence_weight", default="0.5", type=perc_float, help="Audio silence probabilistic weight")
+    arg_parse.add_argument("--silence_window", default="1.0", type=perc_float, help="Audio smoothing window")
+    arg_parse.add_argument("--silence_weight", default="0.3", type=perc_float, help="Audio silence probabilistic weight")
 
     args = arg_parse.parse_args()
 
@@ -39,14 +39,25 @@ if __name__ == "__main__":
 
     # Start transcribing
     print("Transcribing...")
+
+    subs = []
+    for w_file in tqdm(files):
+        start, end, transcription = process_audio(w_file, stt)
+        if len(transcription.strip()) == 0:
+            continue
+
+        subs.append(srt.Subtitle(
+            0,
+            datetime.timedelta(seconds=float(start)),
+            datetime.timedelta(seconds=float(end)),
+            transcription
+        ))
+
+    # write output
+    print(f"Writing subtitle file to {args.output}...")
     with open(args.output, "w", encoding="utf-8") as f:
-
-        for w_file in tqdm(files):
-            start, end, transcription = process_audio(w_file, stt)
-            if len(transcription.strip()) == 0:
-                continue
-
-            write_to_file(f, transcription, 1, (start, end))
+        srt.sort_and_reindex(subs)
+        f.write(srt.compose(subs))
 
     print("Removing temporary files...")
     shutil.rmtree(args.temp_dir)
